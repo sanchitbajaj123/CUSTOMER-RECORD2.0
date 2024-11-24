d294fcab0207dc4b66dd2aa92c4a094121d9419d
sanchit bajaj <sanchitbajaj2003@gmail.com>
2024-11-24 23:53:39 +0530

    sms added

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 
-// const Api="https://customer-record-2-0.vercel.app";
-const Api="http://localhost:5000";
+const Api="https://customer-record-2-0.vercel.app";
+// const Api="http://localhost:5000";
 async function add (data) {
     console.log(data);
     const response = await fetch(Api+"/add-customer", {
